9f64198b37a55b102678c8ef88dc3e499b7f55a0
J <j@Js-Mac.local>
2017-05-15 18:11:25 +0200

    h

--- RoundView/ViewController.swift
+++ RoundView/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var makeThisRound: UIView!
+    override func viewDidLayoutSubviews() {
+        
+        makeThisRound.layer.masksToBounds = true
+        makeThisRound.layer.cornerRadius = 0.5 * makeThisRound.bounds.size.height
+        makeThisRound.clipsToBounds = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
